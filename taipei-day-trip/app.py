fdcc623b15073b391466d7e9deee6dd70abbbbd8
Vivian Lin <vivian.wj.lin@gmail.com>
2022-11-20 22:36:14 +0800

    grap.py added to establish the databases; app.py modified for APIs('/api/attractions','/api/attraction/{attractionId}','/api/categories')

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -1,7 +1,19 @@
+import json
+import flask
+import mysql
+import mysql.connector
+import mysql.connector.cursor
 from flask import *
-app = Flask(__name__)
+from flask import Flask, request
+
+app = Flask(__name__, static_folder="templates", static_url_path="/")
 app.config["JSON_AS_ASCII"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = "secret"
+
+mydb = mysql.connector.connect(
+    host="localhost", user="root", passwd="mysqlpwd2022", database="TaipeiAttractionsDB"
+)
 
 # Pages
 
@@ -26,4 +38,142 @@ def thankyou():
     return render_template("thankyou.html")
 
 
-app.run(port=3000)
+@app.route("/api/categories")
+def api_categories():
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("SELECT DISTINCT category FROM Attractions")
+        data = cursor.fetchall()
+        data = [x[0] for x in data]
+        return {"data": data}
+    except Exception:
+        return Response(
+            json.dumps({"error": True, "message": "Internal Server Error"}),
+            mimetype="application/json",
+            status=500,
+        )
+
+
+@app.route("/api/attractions/<attractionId>")
+def api_attractionId(attractionId):
+    try:
+        att_cursor = mydb.cursor(dictionary=True)
+        sql = """
+                SELECT *
+                FROM Attractions
+                WHERE source_id = %s
+                ;
+                """
+        val = (attractionId,)
+        att_cursor.execute(sql, val)
+        attraction = att_cursor.fetchone()
+
+        if attraction is None:
+            return Response(
+                json.dumps({"error": True, "message": "Wrong attraction ID"}),
+                mimetype="application/json",
+                status=400,
+            )
+
+        # if attractionId exists in database
+        img_cursor = mydb.cursor(dictionary=True)
+        sql = """
+                SELECT *
+                FROM Images
+                WHERE source_id = %s
+                ;
+                """
+        val = (attractionId,)
+        img_cursor.execute(sql, val)
+        images = img_cursor.fetchall()
+        images = [x["images"] for x in images]
+        attraction["images"] = images
+        return {
+            "data": attraction,
+        }
+    except Exception:
+        return Response(
+            json.dumps({"error": True, "message": "Internal Server Error"}),
+            mimetype="application/json",
+            status=500,
+        )
+
+
+@app.route("/api/attractions")
+def api_attractions():
+    try:
+        page = flask.request.args.get("page")
+        keyword = flask.request.args.get("keyword")
+
+        # page is required
+        if page is None:
+            return {"error": True, "message": "Please enter page number"}
+        if page.isdigit() is False:
+            return {"error": True, "message": "Please enter page number"}
+
+        page = int(page)
+
+        # without keywords
+        if keyword is None:
+            att_cursor = mydb.cursor(dictionary=True)
+            sql = """
+                    SELECT source_id,name,category,description,address,transport,mrt,lat,lng 
+                    FROM Attractions
+                    LIMIT 12 OFFSET %s
+                    ;
+                    """
+            val = (page * 12,)
+            att_cursor.execute(sql, val)
+            attractions = att_cursor.fetchall()
+
+        # with keywords
+        else:
+            keyword_cursor = mydb.cursor(dictionary=True)
+            sql = """
+                    SELECT source_id,name,category,description,address,transport,mrt,lat,lng
+                    FROM Attractions
+                    WHERE category = %s or name like CONCAT("%",%s,"%")
+                    LIMIT 12 OFFSET %s
+                    ;
+                    """
+            val = (keyword, keyword, page * 12)
+            keyword_cursor.execute(sql, val)
+            attractions = keyword_cursor.fetchall()
+
+        # composed with images
+        for attraction in attractions:
+            img_cursor = mydb.cursor(dictionary=True)
+            sql = """
+                SELECT images                    
+                FROM Images
+                WHERE source_id = %s
+                ;
+            """
+            val = (attraction["source_id"],)
+            img_cursor.execute(sql, val)
+            images = img_cursor.fetchall()
+            images = [x["images"] for x in images]
+            attraction["images"] = images
+
+        # pagination
+        if len(attractions) == 12:
+            next_page = page+1
+        else:
+            next_page = None
+
+        return {
+            "nextPage": next_page,
+            "data": attractions,
+        }
+
+    except Exception:
+        return Response(
+            json.dumps({"error": True, "message": "Internal Server Error"}),
+            mimetype="application/json",
+            status=500,
+        )
+
+
+app.run(debug=True, port=3000)
+
+# python app.py
---
+++ taipei-day-trip/data/grap.py
@@ -0,0 +1,124 @@
+from flask import *
+from unicodedata import name
+import json
+import mysql.connector
+import mysql.connector.cursor
+app = Flask(__name__,
+            static_folder="templates",
+            static_url_path="/")
+
+file = open("taipei-attractions.json", "r", encoding="UTF-8")
+dict_data = json.load(file)
+attractions_list = dict_data["result"]["results"]
+
+results = []
+for attraction in attractions_list:
+    images_result = []
+    images = str(attraction["file"]).split("https://")
+    for image in images[1: (len(images))]:
+        if image.split(".")[-1].lower() in ["jpg", "png"]:
+            images_result.append("https://" + image)
+    results.append(
+        {
+            "id": attraction["_id"],
+            "name": attraction["name"],
+            "category": attraction["CAT"],
+            "description": attraction["description"],
+            "address": attraction["address"],
+            "transport": attraction["direction"],
+            "mrt": attraction["MRT"],
+            "lat": attraction["latitude"],
+            "lng": attraction["longitude"],
+            "images": images_result,
+        }
+    )
+
+# print(results[0])
+# file.close()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="mysqlpwd2022",
+    database="mysql"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE IF NOT EXISTS TaipeiAttractionsDB")
+mydb.commit()
+mycursor.close()
+mydb.close()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="mysqlpwd2022",
+    database="TaipeiAttractionsDB"
+)
+mycursor = mydb.cursor()
+mycursor.execute(
+    '''
+    CREATE Table IF NOT EXISTS Attractions
+    (
+        id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        source_id int NOT NULL,
+        `name` varchar(100) NOT NULL,
+        category varchar(100) NOT NULL,
+        `description` varchar(5000) NOT NULL,
+        address varchar(500) NOT NULL,
+        transport varchar(1000) NOT NULL,
+        mrt varchar(500),
+        lat int NOT NULL,
+        lng int NOT NULL,
+        INDEX(source_id)
+    );
+    '''
+)
+mydb.commit()
+mycursor = mydb.cursor()
+mycursor.execute(
+    '''
+    CREATE Table IF NOT EXISTS Images
+    (
+        img_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        source_id int NOT NULL,
+        images varchar(10000) NOT NULL,
+        FOREIGN KEY (source_id) REFERENCES Attractions(source_id)
+    );
+    '''
+)
+mydb.commit()
+
+# insert values into table Attractions
+for result in results:
+    source_id = (result["id"])
+    name = (result["name"])
+    category = (result["category"])
+    description = (result["description"])
+    address = (result["address"])
+    transport = (result["transport"])
+    mrt = (result["mrt"])
+    lat = (result["lat"])
+    lng = (result["lng"])
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO Attractions (source_id,name, category, description,address,transport,mrt,lat,lng) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = [(source_id, name, category, description,
+            address, transport, mrt, lat, lng)]
+    mycursor.executemany(sql, val)
+    mydb.commit()
+
+# insert values into table Images
+for result in results:
+    source_id = (result["id"])
+    images = (result["images"])
+    for image in images:
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO Images (source_id,images) VALUES (%s, %s)"
+        val = [(source_id, image)]
+        mycursor.executemany(sql, val)
+        mydb.commit()
+
+mycursor.close()
+mydb.close()
+file.close()
+# python grap.py
