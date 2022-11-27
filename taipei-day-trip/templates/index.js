9cecaa4281fe03cf3294ac7b12818fce81033f66
Vivian Lin <vivian.wj.lin@gmail.com>
2022-11-27 22:00:57 +0800

    commit message

--- taipei-day-trip/templates/index.js
+++ taipei-day-trip/templates/index.js
@@ -27,7 +27,7 @@ fetch("http://127.0.0.1:5500/taipei-day-trip/data/taipei-attractions.json")
     //name and img
     const container = document.querySelector(".container")
     let j = 0 //景點從0開始
-    function appendAttraction(){   
+    function appendAttraction(){
       //attractions
       const divAttractions = document.createElement("div")
       divAttractions.className = "attractions"
@@ -65,9 +65,59 @@ fetch("http://127.0.0.1:5500/taipei-day-trip/data/taipei-attractions.json")
     }
 
     for (let i = 0; i < 12; i++) {
+      isloading=true;
       appendAttraction()
-    }  
+      isloading=false; 
+      // break;
+    }
+    
+    document.addEventListener('scroll', (ev) => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+      if (window.innerHeight >= document.querySelector('html').getBoundingClientRect().bottom) {
+        console.log('end')
+        // ....
+        appendAttraction()
+      }
+    })
 
+//     window.onscroll = function(ev) {
+//           if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+//             console.log('end')
+//             window.setTimeout(appendAttraction, 3000)
+//     }
+// };
+
+    //search
+    const searchForm = document.querySelector('.searchbar')
+    const searchInput = document.querySelector('.searchinput')
+    searchForm.addEventListener('submit', function search(event)  {
+    event.preventDefault()
+    const keyword = searchInput.value
+    // console.log(keyword) 
+    let filteredresults = []
+    // console.log(attractions)
+
+    for(let attraction of attractions){
+    // console.log(attraction);
+      if(attraction.attractionName.includes(keyword)||attraction.cat.includes(keyword)){
+        filteredresults.push(attraction)
+      }
+      // console.log(filteredresults);//搜尋"地熱谷"，印出58個地熱谷...
+      appendAttraction(filteredresults);
+      //index.js:40 Uncaught TypeError: Cannot read properties of undefined (reading 'fistImage')
+      // at appendAttraction (index.js:40:32)
+      // at HTMLFormElement.search (index.js:107:7)
+    }
+  }) 
 })
+
+
+
+    
+
+
+
+
+
     
   
