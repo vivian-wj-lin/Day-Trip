395f56e6af72d80b47d729cbba5b7e74bb8784ca
Vivian Lin <vivian.wj.lin@gmail.com>
2022-12-26 03:37:16 +0800

    mysql user changed

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -27,10 +27,10 @@ cnxpool = pooling.MySQLConnectionPool(
     pool_size=10,
     host='localhost',
     database='TaipeiAttractionsDB',
-    user='root',
-    # user="debian-sys-maint",
+    # user='root',
+    user="debian-sys-maint",
     password='mysqlpwd2022'
-    # passwd="b6hdV6hWNuqadE2s",
+    passwd="b6hdV6hWNuqadE2s",
     # auth_plugin='mysql_native_password'
 )
 
